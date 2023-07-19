7106728e447d5f9e4b09e47c775022b8a4608423
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-07-19 16:16:41 +0300

    Update SMS navbar theme

--- src/pages/RigelSMS.vue
+++ src/pages/RigelSMS.vue
@@ -8,6 +8,14 @@
 import Hero from '../views/SMS/Hero.vue';
 import Feature from '../views/SMS/Feature.vue';
 import Contact from "../views/SMS//Contact.vue";
+import {onMounted} from "vue";
+import {useNavBarStore} from "../store/navbar_store.js";
+
+const navBarStore = useNavBarStore();
+onMounted(() => {
+  navBarStore.setNavBarTheme('bg-purple-600 text-purple-50', 'text-purple-50 fill-purple-50', 'text-amber-400');
+
+})
 </script>
 <style scoped>
 </style>
