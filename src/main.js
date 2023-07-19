fe730bd2545a3a6a0236f1d0d5938ef633afc4fb
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-07-19 16:55:31 +0300

    Add touch events package

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import 'aos/dist/aos.css'
 import 'boxicons'
 import router from './routes.js'
 import {createPinia} from "pinia";
+import Vue3TouchEvents from "vue3-touch-events";
 
 createApp(App)
     .use(router)
     .use(createPinia())
+    .use(Vue3TouchEvents)
     .mount('#app');
