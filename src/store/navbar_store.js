4c1d1d0fd69b1bc882368bce177ce2a8fca70070
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-09-27 22:26:15 +0300

    Update theme and fix style bugs

--- src/store/navbar_store.js
+++ src/store/navbar_store.js
@@ -1,92 +1,146 @@
 import { defineStore } from "pinia";
 
 export const useNavBarStore = defineStore({
-    id: "navbar",
-    state: () => ({
-        style: 'bg-black text-black',
-        secondaryStyle: 'fill-gray-700 text-gray-100',
-        logoStyle:"text-primary",
-        menuStyle: 'bg-white text-black',
-        menuButtonStyle: 'fill-primary',
-        showNavBar: false,
-        showMenu: false,
-        selectedProject: 1,
-        currentPage: "home"
-    }),actions: {
-        toggleNavBar() {
-            this.showNavBar = !this.showNavBar;
-        },
-        toggleMenu() {
-            this.showMenu = !this.showMenu;
-        },
-        setProjectsNavBarTheme() {
-            switch (this.selectedProject) {
-                case 1:
-                    this.setNavBarTheme('bg-purple-600 text-purple-50', 'text-purple-50 fill-purple-50', 'text-amber-400');
-                    this.setMenuProperty('bg-purple-600 text-purple-50', 'fill-[#B8BFC6]');
-                    break;
-                case 2:
-                    this.setNavBarTheme('bg-gray-700 text-gray-50', 'text-gray-100 fill-gray-50', 'text-amber-400');
-                    this.setMenuProperty('bg-gray-700 text-gray-50', 'fill-gray-50');
-                    break;
-                case 3:
-                    this.setNavBarTheme('bg-blue-700 text-blue-50', 'text-blue-50 fill-blue-50', 'text-amber-400');
-                    this.setMenuProperty('bg-blue-700 text-blue-50 ', 'fill-orange-50');
-                    break;
-                case 4:
-                    this.setNavBarTheme('bg-orange-700 text-orange-50', 'text-white fill-white', 'text-orange-200');
-                    this.setMenuProperty('bg-orange-700 text-orange-50', 'fill-orange-50');
-                    break;
+  id: "navbar",
+  state: () => ({
+    style: "bg-black text-black",
+    secondaryStyle: "fill-gray-500 text-gray-100",
+    logoStyle: "text-primary",
+    menuStyle: "bg-white text-black",
+    menuButtonStyle: "fill-primary",
+    showNavBar: false,
+    showMenu: false,
+    selectedProject: 1,
+    currentPage: "home",
+  }),
+  actions: {
+    toggleNavBar() {
+      this.showNavBar = !this.showNavBar;
+    },
+    toggleMenu() {
+      this.showMenu = !this.showMenu;
+    },
+    setProjectsNavBarTheme() {
+      switch (this.selectedProject) {
+        case 1:
+          this.setNavBarTheme(
+            "bg-purple-600 text-purple-50",
+            "text-purple-50 fill-purple-50",
+            "text-amber-400",
+          );
+          this.setMenuProperty(
+            "bg-purple-600 text-purple-50",
+            "fill-[#B8BFC6]",
+          );
+          break;
+        case 2:
+          this.setNavBarTheme(
+            "bg-gray-700 text-gray-50",
+            "text-gray-100 fill-gray-50",
+            "text-amber-400",
+          );
+          this.setMenuProperty("bg-gray-700 text-gray-50", "fill-gray-50");
+          break;
+        case 3:
+          this.setNavBarTheme(
+            "bg-blue-700 text-blue-50",
+            "text-blue-50 fill-blue-50",
+            "text-amber-400",
+          );
+          this.setMenuProperty("bg-blue-700 text-blue-50 ", "fill-orange-50");
+          break;
+        case 4:
+          this.setNavBarTheme(
+            "bg-orange-700 text-orange-50",
+            "text-white fill-white",
+            "text-orange-200",
+          );
+          this.setMenuProperty(
+            "bg-orange-700 text-orange-50",
+            "fill-orange-50",
+          );
+          break;
 
-                case 5:
-                    this.setNavBarTheme('bg-red-600 text-red-50', 'text-red-50 fill-red-50', 'text-[#2E2E2E]');
-                    this.setMenuProperty('bg-red-600 text-red-50', 'fill-red-50');
-                    break;
-                case 6:
-                    this.setNavBarTheme('bg-white text-black', 'text-gray-800 fill-gray-800', 'text-primary');
-                    this.setMenuProperty('bg-white text-black', 'fill-black');
-                    break;
-                case 7:
-                    this.setNavBarTheme('bg-yellow-600 text-yellow-50', 'text-yellow-50 fill-yellow-50', 'text-[#37323E]');
-                    this.setMenuProperty('bg-yellow-600 text-yellow-50', 'fill-yellow-50');
-                    break;
-                default:
-                    break;
-            }
-        },
-        setNavBarTheme(style, secondaryStyle, logoStyle) {
-            this.style = style;
-            this.secondaryStyle = secondaryStyle;
-            this.logoStyle = logoStyle;
-        },
-        setMenuTheme() {
-            switch (this.currentPage) {
-                case "home":
-                    this.setMenuProperty('bg-white text-black', 'fill-primary');
-                    this.setNavBarTheme('bg-white text-black', 'text-black fill-black', 'text-primary');
-                    break;
-                case "about":
-                    this.setMenuProperty('bg-gray-100 text-black', 'fill-black');
-                    this.setNavBarTheme('bg-gray-100 text-gray-700', 'text-gray-800 fill-gray-800', 'text-primary');
-                    break;
-                case "projects":
-                     this.setProjectsNavBarTheme()
-                    break;
-                case "team":
-                    this.setMenuProperty('bg-gray-100 text-black', 'fill-black');
-                    this.setNavBarTheme('bg-gray-100 text-gray-800', 'text-gray-800 fill-gray-800', 'text-primary');
-                    break;
-                case "contact":
-                    this.setNavBarTheme('bg-gray-100 text-gray-800', 'text-gray-800 fill-gray-800', 'text-primary');
-                    this.setMenuProperty('bg-gray-100 text-black', 'fill-black');
-                    break;
-                default:
-                    break;
-            }
-        },
-        setMenuProperty(menuStyle, menuButtonStyle) {
-            this.menuStyle = menuStyle;
-            this.menuButtonStyle = menuButtonStyle + ' w-10';
-        },
-    }
+        case 5:
+          this.setNavBarTheme(
+            "bg-red-600 text-red-50",
+            "text-red-50 fill-red-50",
+            "text-[#2E2E2E]",
+          );
+          this.setMenuProperty("bg-red-600 text-red-50", "fill-red-50");
+          break;
+        case 6:
+          this.setNavBarTheme(
+            "bg-white text-black",
+            "text-gray-800 fill-gray-800",
+            "text-primary",
+          );
+          this.setMenuProperty("bg-white text-black", "fill-black");
+          break;
+        case 7:
+          this.setNavBarTheme(
+            "bg-yellow-600 text-yellow-50",
+            "text-yellow-50 fill-yellow-50",
+            "text-[#37323E]",
+          );
+          this.setMenuProperty(
+            "bg-yellow-600 text-yellow-50",
+            "fill-yellow-50",
+          );
+          break;
+        default:
+          break;
+      }
+    },
+    setNavBarTheme(style, secondaryStyle, logoStyle) {
+      this.style = style;
+      this.secondaryStyle = secondaryStyle;
+      this.logoStyle = logoStyle;
+    },
+    setMenuTheme() {
+      switch (this.currentPage) {
+        case "home":
+          this.setMenuProperty("bg-black text-gray-100", "fill-primary");
+          this.setNavBarTheme(
+            "bg-black text-gray-100",
+            "text-white fill-gray-500",
+            "text-primary",
+          );
+          break;
+        case "about":
+          this.setMenuProperty("bg-gray-100 text-black", "fill-black");
+          this.setNavBarTheme(
+            "bg-gray-100 text-gray-700",
+            "text-gray-800 fill-gray-800",
+            "text-primary",
+          );
+          break;
+        case "projects":
+          this.setProjectsNavBarTheme();
+          break;
+        case "team":
+          this.setMenuProperty("bg-gray-100 text-black", "fill-black");
+          this.setNavBarTheme(
+            "bg-gray-100 text-gray-800",
+            "text-gray-800 fill-gray-800",
+            "text-primary",
+          );
+          break;
+        case "contact":
+          this.setNavBarTheme(
+            "bg-gray-100 text-gray-800",
+            "text-gray-800 fill-gray-800",
+            "text-primary",
+          );
+          this.setMenuProperty("bg-gray-100 text-black", "fill-black");
+          break;
+        default:
+          break;
+      }
+    },
+    setMenuProperty(menuStyle, menuButtonStyle) {
+      this.menuStyle = menuStyle;
+      this.menuButtonStyle = menuButtonStyle + " w-10";
+    },
+  },
 });
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,55 +1,68 @@
 <template>
   <div class="bg-gray-50 relative">
-
     <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 z-20">
       <div class="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
         <div class="lg:col-span-2 lg:py-12">
-
           <div class="py-6 md:py-0 md:px-6">
-            <h1 class="text-4xl font-bold">Get in <span class="border-b-4 border-primary cursor-pointer">Touch</span></h1>
-            <p class="pt-2 pb-4">Contact us today. Let's connect and discuss how we can assist you. Get in touch now!</p>
+            <h1 class="text-4xl font-bold">
+              Get in
+              <span class="border-b-4 border-primary cursor-pointer"
+                >Touch</span
+              >
+            </h1>
+            <p class="pt-2 pb-4">
+              Contact us today. Let's connect and discuss how we can assist you.
+              Get in touch now!
+            </p>
 
-          <ContactInfo></ContactInfo>
-
+            <ContactInfo></ContactInfo>
           </div>
         </div>
-        <div class=" rounded-l max-h-[60vh] z-20 bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
-          <form @submit.prevent="submitForm" class="space-y-4">
+        <div
+          class="rounded-l z-20 p-5 bg-white shadow-sm lg:col-span-3 lg:p-12"
+        >
+          <form class="space-y-4" @submit.prevent="submitForm">
             <div>
               <label class="sr-only" for="name">Name</label>
               <input
-                  v-model="formData.name"
-                  class="w-full rounded-lg border border-primary/20 !outline-none p-3 text-sm"
-                  placeholder="Name"
-                  type="text"
-                  id="name"
+                id="name"
+                v-model="formData.name"
+                class="w-full rounded-lg border border-primary/20 !outline-none p-3 text-sm"
+                placeholder="Name"
+                type="text"
               />
-              <div v-if="showNameError" class="text-red-500 text-sm mt-1">{{ nameError }}</div>
+              <div v-if="showNameError" class="text-red-500 text-sm mt-1">
+                {{ nameError }}
+              </div>
             </div>
 
             <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
               <div>
                 <label class="sr-only" for="email">Email</label>
                 <input
-                    v-model="formData.email"
-                    class="w-full border border-primary/20 !outline-none rounded-lg p-3 text-sm"
-                    placeholder="Email address"
-                    type="email"
-                    id="email"
+                  id="email"
+                  v-model="formData.email"
+                  class="w-full border border-primary/20 !outline-none rounded-lg p-3 text-sm"
+                  placeholder="Email address"
+                  type="email"
                 />
-                <div v-if="showEmailError" class="text-red-500 text-sm mt-1">{{ emailError }}</div>
+                <div v-if="showEmailError" class="text-red-500 text-sm mt-1">
+                  {{ emailError }}
+                </div>
               </div>
 
               <div>
                 <label class="sr-only" for="phone">Phone</label>
                 <input
-                    v-model="formData.phone_no"
-                    class="border border-primary/20 !outline-none w-full rounded-lg p-3 text-sm"
-                    placeholder="Phone Number"
-                    type="tel"
-                    id="phone"
+                  id="phone"
+                  v-model="formData.phone_no"
+                  class="border border-primary/20 !outline-none w-full rounded-lg p-3 text-sm"
+                  placeholder="Phone Number"
+                  type="tel"
                 />
-                <div v-if="showPhoneError" class="text-red-500 text-sm mt-1">{{ phoneError }}</div>
+                <div v-if="showPhoneError" class="text-red-500 text-sm mt-1">
+                  {{ phoneError }}
+                </div>
               </div>
             </div>
 
@@ -57,19 +70,21 @@
               <label class="sr-only" for="message">Message</label>
 
               <textarea
-                  v-model="formData.content"
-                  class="w-full rounded-lg border border-primary/20 !outline-none p-3 text-sm"
-                  placeholder="Message"
-                  rows="8"
-                  id="message"
+                id="message"
+                v-model="formData.content"
+                class="w-full rounded-lg border border-primary/20 !outline-none p-3 text-sm"
+                placeholder="Message"
+                rows="8"
               ></textarea>
-              <div v-if="showMessageError" class="text-red-500 text-sm mt-1">{{ messageError }}</div>
+              <div v-if="showMessageError" class="text-red-500 text-sm mt-1">
+                {{ messageError }}
+              </div>
             </div>
 
             <div class="mt-4">
               <button
-                  type="submit"
-                  class="shadow-md font-medium my-5 py-2 px-4 text-white cursor-pointer bg-primary hover:bg-primary rounded text-lg text-center w-32"
+                class="shadow-md font-medium my-5 py-2 px-4 text-white cursor-pointer bg-primary hover:bg-primary rounded text-lg text-center w-32"
+                type="submit"
               >
                 Send
               </button>
@@ -77,123 +92,141 @@
           </form>
         </div>
       </div>
-
     </div>
-
 
     <!-- notification  -->
     <div class="relative">
-      <div v-if="showNotification" class="bg-white/20  backdrop-blur-xl z-20 max-w-md absolute right-5 bottom-1 rounded-lg p-3 shadow-lg">
-        <button @click="showNotification = !showNotification" type="button" class="absolute top-0 right-1.5 text-primary bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="authentication-modal">
-          <svg aria-hidden="true" class="w-5 h-5" fill="black" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+      <div
+        v-if="showNotification"
+        class="bg-white/20 backdrop-blur-xl z-20 max-w-md absolute right-5 bottom-1 rounded-lg p-3 shadow-lg"
+      >
+        <button
+          class="absolute top-0 right-1.5 text-primary bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+          data-modal-hide="authentication-modal"
+          type="button"
+          @click="showNotification = !showNotification"
+        >
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5"
+            fill="black"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              clip-rule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              fill-rule="evenodd"
+            ></path>
+          </svg>
         </button>
-        <h1 class="text-md text-slate-700 font-medium pt-3 mr-2">Thank you,{{name}}! We've received your message and will be in touch soon. We appreciate your interest in working with us.</h1>
+        <h1 class="text-md text-slate-700 font-medium pt-3 mr-2">
+          Thank you,{{ name }}! We've received your message and will be in touch
+          soon. We appreciate your interest in working with us.
+        </h1>
       </div>
     </div>
-
-
   </div>
 </template>
 
 <script setup>
-import { ref} from 'vue';
+import { ref } from "vue";
 import ContactInfo from "../components/ContactInfo.vue";
 import axios from "axios";
 
-
-const showNotification = ref(false)
+const showNotification = ref(false);
 const showNameError = ref(false);
 const showEmailError = ref(false);
 const showPhoneError = ref(false);
 const showMessageError = ref(false);
-const nameError = ref('');
-const emailError = ref('');
-const phoneError = ref('');
-const messageError = ref('');
-const name = ref()
-const response = ref()
-const formData= ref({
-  name:'',
-  email:'',
-  phone_no:'',
-  content:''
-})
-
+const nameError = ref("");
+const emailError = ref("");
+const phoneError = ref("");
+const messageError = ref("");
+const name = ref();
+const response = ref();
+const formData = ref({
+  name: "",
+  email: "",
+  phone_no: "",
+  content: "",
+});
 
 function validateForm() {
   let valid = true;
   // Validate name
   if (!formData.value.name.trim()) {
-    nameError.value = 'Please enter your name.';
+    nameError.value = "Please enter your name.";
     showNameError.value = true;
     valid = false;
   } else {
-    nameError.value = '';
+    nameError.value = "";
     showNameError.value = false;
   }
 
   // Validate email
   if (!formData.value.email.trim()) {
-    emailError.value = 'Please enter your email address.';
+    emailError.value = "Please enter your email address.";
     showEmailError.value = true;
     valid = false;
   } else if (!/\S+@\S+\.\S+/.test(formData.value.email)) {
-    emailError.value = 'Please enter a valid email address.';
+    emailError.value = "Please enter a valid email address.";
     showEmailError.value = true;
     valid = false;
   } else {
-    emailError.value = '';
+    emailError.value = "";
     showEmailError.value = false;
   }
 
-// Validate phone
+  // Validate phone
   if (!formData.value.phone_no.trim()) {
-    phoneError.value = 'Please enter your phone number.';
+    phoneError.value = "Please enter your phone number.";
     showPhoneError.value = true;
     valid = false;
   } else {
-    phoneError.value = '';
+    phoneError.value = "";
     showPhoneError.value = false;
   }
 
-// Validate message
+  // Validate message
   if (!formData.value.content.trim()) {
-    messageError.value = 'Please enter your message.';
+    messageError.value = "Please enter your message.";
     showMessageError.value = true;
     valid = false;
   } else {
-    messageError.value = '';
+    messageError.value = "";
     showMessageError.value = false;
   }
   return valid;
 }
 
-
-
 function submitForm() {
   if (validateForm()) {
-    axios.post(`https://api.telegram.org/bot${import.meta.env.VITE_TOKEN}/sendMessage?chat_id=${import.meta.env.VITE_CHATID}&text=Name: ${formData.value.name}%0AEmail: ${formData.value.email}%0A%0A${formData.value.content}%0APhone: ${formData.value.phone_no}%0A`)
-        .then(r => {
-          response.value = r.data
-          showNotification.value = true
-          setTimeout(() => {
-            showNotification.value = false;
-          }, 7000);
-          name.value = formData.value.name
-          formData.value.name = '';
-          formData.value.email = '';
-          formData.value.phone_no = '';
-          formData.value.content = '';
-
-        })
-        .catch(error => {
-          console.log(error)
-        })
-
-  };
-
-
-
-
+    axios
+      .post(
+        `https://api.telegram.org/bot${
+          import.meta.env.VITE_TOKEN
+        }/sendMessage?chat_id=${import.meta.env.VITE_CHATID}&text=Name: ${
+          formData.value.name
+        }%0AEmail: ${formData.value.email}%0A%0A${
+          formData.value.content
+        }%0APhone: ${formData.value.phone_no}%0A`,
+      )
+      .then((r) => {
+        response.value = r.data;
+        showNotification.value = true;
+        setTimeout(() => {
+          showNotification.value = false;
+        }, 7000);
+        name.value = formData.value.name;
+        formData.value.name = "";
+        formData.value.email = "";
+        formData.value.phone_no = "";
+        formData.value.content = "";
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
 </script>
